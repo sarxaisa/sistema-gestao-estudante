2a2f1352b0201738d5f604df3e2094de5679bd0e
sarxaisa <118470659+sarxaisa@users.noreply.github.com>
2023-05-30 17:22:09 -0300

    Atualizado

---
+++ sistema-gestao-estudante/Login.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace sistema_gestao_estudante
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Usuário = new System.Windows.Forms.Label();
+            this.Senha = new System.Windows.Forms.Label();
+            this.Entrar = new System.Windows.Forms.Button();
+            this.Cancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(49, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 229);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Usuário
+            // 
+            this.Usuário.AutoSize = true;
+            this.Usuário.Location = new System.Drawing.Point(25, 189);
+            this.Usuário.Name = "Usuário";
+            this.Usuário.Size = new System.Drawing.Size(43, 13);
+            this.Usuário.TabIndex = 3;
+            this.Usuário.Text = "Usuário";
+            // 
+            // Senha
+            // 
+            this.Senha.AutoSize = true;
+            this.Senha.Location = new System.Drawing.Point(25, 229);
+            this.Senha.Name = "Senha";
+            this.Senha.Size = new System.Drawing.Size(38, 13);
+            this.Senha.TabIndex = 4;
+            this.Senha.Text = "Senha";
+            this.Senha.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Entrar
+            // 
+            this.Entrar.Location = new System.Drawing.Point(28, 295);
+            this.Entrar.Name = "Entrar";
+            this.Entrar.Size = new System.Drawing.Size(82, 37);
+            this.Entrar.TabIndex = 5;
+            this.Entrar.Text = "Entrar";
+            this.Entrar.UseVisualStyleBackColor = true;
+            this.Entrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Cancelar
+            // 
+            this.Cancelar.Location = new System.Drawing.Point(129, 295);
+            this.Cancelar.Name = "Cancelar";
+            this.Cancelar.Size = new System.Drawing.Size(82, 37);
+            this.Cancelar.TabIndex = 6;
+            this.Cancelar.Text = "Cancelar";
+            this.Cancelar.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(239, 450);
+            this.Controls.Add(this.Cancelar);
+            this.Controls.Add(this.Entrar);
+            this.Controls.Add(this.Senha);
+            this.Controls.Add(this.Usuário);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Login";
+            this.Text = "SGE- entrar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Usuário;
+        private System.Windows.Forms.Label Senha;
+        private System.Windows.Forms.Button Entrar;
+        private System.Windows.Forms.Button Cancelar;
+    }
+}
+
---
+++ sistema-gestao-estudante/Meu_BD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace sistema_gestao_estudante
+{
+    internal class Meu_BD
+    {
+        MySqlConnection conexao = new MySqlConnection("datasource=localhost;username=root;password=;database=sga_estudantes_bd_T4");
+    }
+}
